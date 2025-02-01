f711716a901217e09e040730e62b2cb41e880cb7
nestor_pers_pc <nestor.chulvi@gmail.com>
2025-02-01 17:07:34 +0100

    [app] Adapta to sql database

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template
-import pandas as pd
 import numpy as np
+import sqlite3
 
 app = Flask(__name__)
 
+con = sqlite3.connect("./posts.db",check_same_thread=False)
+cur = con.cursor()
+
 @app.route("/")
 def index():
-    posts = np.flip(pd.read_csv("./posts_df.csv").to_records())
+    posts = np.flip(cur.execute("SELECT * FROM posts").fetchall(),axis = 0)
     return render_template("index.html", posts = posts)
 
 @app.route("/about")
@@ -15,5 +18,5 @@ def about():
 
 @app.route("/post/<int:post_id>")
 def post(post_id):
-    post = pd.read_csv("./posts_df.csv").to_records()[post_id]
+    post = cur.execute("SELECT * FROM posts WHERE id = ?",[post_id]).fetchone()
     return render_template("post.html", post = post)
