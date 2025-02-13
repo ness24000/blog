b8a232e1066505da8d25eb9d08221a44da551902
nestor_pers_pc <nestor.chulvi@gmail.com>
2025-02-13 17:35:47 +0100

    [add-post] Improve title/date detection and image replacement
    I use regular expression to make these processes more robust or
    better (e.g. keep original altext in path to image replacement)

--- add_post.py
+++ add_post.py
@@ -1,12 +1,23 @@
 import markdown
 import typer
+import re
+
+IMG_PATTERN = r"!\[.*\]\("
 
 def main(file_name:str):
 
     with open(file_name, "r") as file: 
-        title, date, content = file.read().split("\n\n",2)
+        title, date, content = re.split("\n+",file.read(), maxsplit=2)
 
-    content = content.replace("![](", f"![an image](../static/{title.lower().replace(" ", "_")}/")
+    img_folder = title.lower().replace(" ", "_")
+    def produce_image_path(matchobj, img_folder = img_folder):
+        return matchobj.group(0) + f"../static/{img_folder}/"
+    
+    content = re.sub(IMG_PATTERN, produce_image_path, content)
+
+    print("title", title)
+    print("date", date)
+    print("content", content)
 
 
 if __name__ == "__main__":
