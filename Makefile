1ad61e4533e560d4732607a8240ecfc120314ad3
nestor-dell <nestor.chulvi@gmail.com>
2025-04-05 17:35:06 +0200

    [env] Replace dotenv with config.py
    Since 8bc54e8, we make extensive use of config.py, which is a standard
    flask configuration file. Since it is can define default values for
    config variables the example.env and subsequent .env logic seems
    unnecessary (i.e. the role of example.env is fulfilled by the defaults
    and the .env needs to be written from scratch anyway).

--- Makefile
+++ Makefile
@@ -1,5 +1,2 @@
 create_database:
 	python scripts/create_database.py
-
-create_env:
-	cp example.env .env
--- config.py
+++ config.py
@@ -1,11 +1,6 @@
 import os
 
-from dotenv import load_dotenv
-
-load_dotenv()
-
-
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY")
-    LOG_LEVEL = os.getenv("LOG_LEVEL") or "WARNING"
-    PATH_TO_DB = os.getenv("PATH_TO_DB")
+    SECRET_KEY = os.getenv("SECRET_KEY") or "my_secret_key"
+    LOG_LEVEL = os.getenv("LOG_LEVEL") or "DEBUG"
+    PATH_TO_DB = os.getenv("PATH_TO_DB") or "./posts.db"
