372f92e5e5802876dcc0c5522cb63ee5a1fbe55a
nestor-dell <nestor.chulvi@gmail.com>
2025-04-06 18:48:41 +0200

    [db] Initialize database on app start up
    If the table already exists in a posts.db file in the correct
    location, it is used as the posts table. The only issue with this is
    if the table does not satisfy the necessary schema, in which case an
    internal server error will show up when a post gets added. It would be
    nice to check this in the future.

--- app/posts_management.py
+++ app/posts_management.py
@@ -1,14 +1,33 @@
+import logging
+import os
 import re
 import sqlite3
 
 import markdown
 
 
-def connect_to_db(path_to_db:str):
+def initialize_db(path_to_db: str):
+
+    con = sqlite3.connect(path_to_db)
+    cur = con.cursor()
+
+    sql_create_posts = """CREATE TABLE IF NOT EXISTS posts (
+       id INTEGER PRIMARY KEY NOT NULL,
+       title VARCHAR NOT NULL,
+       date VARCHAR NOT NULL,
+       content VARCHAR NOT NULL,
+       preview VARCHAR NOT NULL);"""
+
+    cur.executescript(sql_create_posts)
+    con.commit()
+
+
+def connect_to_db(path_to_db: str):
     con = sqlite3.connect(path_to_db, check_same_thread=False)
     return con.cursor()
 
-def add_post_to_db(title:str, date:str, content:str, path_to_db:str):
+
+def add_post_to_db(title: str, date: str, content: str, path_to_db: str):
 
     IMG_PATTERN = r"!\[.*\]\("
     img_folder = title.lower().replace(" ", "_")
