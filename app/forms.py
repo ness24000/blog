9d17e7ae8f604fd1e0aadf04fdad74181b74cec2
nestor-dell <nestor.chulvi@gmail.com>
2025-04-06 18:32:00 +0200

    [forms] Make date required

--- app/forms.py
+++ app/forms.py
@@ -4,6 +4,6 @@ from wtforms import PasswordField, StringField, TextAreaField, validators
 
 class AddPostForm(FlaskForm):
     title = StringField("Title", [validators.input_required()])
-    date = StringField("Date")
+    date = StringField("Date", [validators.input_required()])
     content = TextAreaField("Content", [validators.input_required()])
     admin_key = PasswordField("Admin Key", [validators.input_required()])
