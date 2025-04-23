aa52e907aa0419330ceea39a6a0da07400d059e8
nestor <nestor.chulvi@gmail.com>
2025-04-23 17:07:38 +0200

    [app] Add edit_post page
    Accessible as {url}/edit_post/{post_id}

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,7 @@ from flask import jsonify, redirect, render_template, request
 from werkzeug.security import check_password_hash
 
 from app import app, cur, logger
-from app.db_interface import add_post_to_db
+from app.db_interface import add_post_to_db, update_post_in_db
 from app.forms import AddPostForm
 from app.input_processing import format_post_input
 
@@ -57,3 +57,39 @@ def add_post():
         return redirect("/")
 
     return render_template("add_post.html", form=form)
+
+
+@app.route("/edit_post/<int:post_id>", methods=["GET", "POST"])
+def edit_post(post_id):
+
+    form = AddPostForm()
+
+    # POST: format input run as in add post, use app.db_interface.update_post_in_db
+    if form.validate_on_submit():
+        logger.debug(f"Updating post {post_id}  with new title {form.title.data}")
+
+        title, date, preview_md, content_md, preview_html, content_html = (
+            format_post_input(form.title.data, form.preview.data, form.content.data)
+        )
+        update_post_in_db(
+            post_id,
+            title,
+            date,
+            preview_md,
+            content_md,
+            preview_html,
+            content_html,
+            app.config["PATH_TO_DB"],
+        )
+
+        return redirect("/")
+
+    # GET: return the edit_post.html with filled fields
+    pos_id, title, date, preview_md, content_md = cur.execute(
+        "SELECT id, title, date, preview_md, content_md FROM posts WHERE id=?",
+        [post_id],
+    ).fetchone()
+    form = AddPostForm(title=title, date=date, content=content_md, preview=preview_md)
+
+    return render_template("add_post.html", form=form, update=True)
+
