96d7547f9394f0d0b12dbb8c7c5157041cdf4324
nestor <nestor.chulvi@gmail.com>
2025-08-31 16:44:35 +0200

    [mail] Initial config

--- config.py
+++ config.py
@@ -8,3 +8,10 @@ class Config:
     LOG_LEVEL = os.getenv("LOG_LEVEL") or "DEBUG"
     PATH_TO_DB = os.getenv("PATH_TO_DB") or "./data/posts.db"
     ADMIN_KEY_HASH = generate_password_hash(os.getenv("ADMIN_KEY"))
+    
+    MAIL_SERVER = os.getenv("MAIL_SERVER")
+    MAIL_PORT = int(os.getenv("MAIL_PORT"))
+    MAIL_USERNAME = os.getenv("MAIL_USERNAME")
+    MAIL_PASSWORD = os.getenv("MAILTRAP_API_KEY")
+    MAIL_USE_TLS = True
+    MAIL_USE_SSL = False
