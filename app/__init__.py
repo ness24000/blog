ca8bd9c566ead08aad306d625762f11f8c75dfaf
nestor <nestor.chulvi@gmail.com>
2025-12-06 18:44:03 +0100

    [celery] Initialize celery app instance

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask import Flask
 from flask_mail import Mail
 from werkzeug.middleware.proxy_fix import ProxyFix
 
+from app.celery_init_app import celery_init_app
 from app.db_interface import connect_to_db, initialize_db
 from app.utils import get_logger
 from config import Config
@@ -12,6 +13,7 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 mail = Mail(app)    
+celery_app = celery_init_app(app)
 
 # necessary if app behind reverse proxy
 app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1)
@@ -24,7 +26,6 @@ initialize_db(app.config["PATH_TO_DB"])
 cur = connect_to_db(app.config["PATH_TO_DB"])
 
 
-from app import routes
-from app import errorhandlers
+from app import errorhandlers, routes
 
 logger.debug(f"Blog initialization finished")
