7922315d1ab3063fc1a5dd07c3d3b7b0788395ed
nestor_ubuntu <nestor.chulvi@gmail.com>
2026-01-30 11:40:13 +0100

    [celery] Document entrypoint command

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
+
+# start a celery worker in the background 
 celery multi start w1 -A app:celery_app -l INFO
+
+# start flask application via gunicorn, publicly (i.e. 0.0.0.0:...)
 gunicorn -b 0.0.0.0:8080 blog_app:app
