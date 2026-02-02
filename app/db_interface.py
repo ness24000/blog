af529011e7b2a6e0a8b0cbf10b3be3b214028f3d
nestor_ubuntu <nestor.chulvi@gmail.com>
2026-02-02 13:07:54 +0100

    [oop] Remove redundant functions

--- app/db_interface.py
+++ app/db_interface.py
@@ -1,37 +1,6 @@
 import sqlite3
 from app.utils import get_date
 
-
-def initialize_db(path_to_db: str):
-
-    con = sqlite3.connect(path_to_db)
-    cur = con.cursor()
-
-    sql_create_posts = """CREATE TABLE IF NOT EXISTS posts (
-       id INTEGER PRIMARY KEY NOT NULL,
-       title VARCHAR NOT NULL,
-       date VARCHAR NOT NULL,
-       content_md VARCHAR NOT NULL,
-       preview_md VARCHAR NOT NULL,
-       content_html VARCHAR NOT NULL,
-       preview_html VARCHAR NOT NULL);"""
-    cur.executescript(sql_create_posts)
-
-    sql_create_email = """CREATE TABLE IF NOT EXISTS email (
-       id INTEGER PRIMARY KEY NOT NULL,
-       date VARCHAR NOT NULL,
-       email_address VARCHAR NOT NULL UNIQUE,
-       confirmed VARCHAR NOT NULL);"""
-    cur.executescript(sql_create_email)
-
-    con.commit()
-
-
-def connect_to_db(path_to_db: str):
-    con = sqlite3.connect(path_to_db, check_same_thread=False)
-    return con.cursor()
-
-
 def add_post_to_db(
     title: str,
     date: str,
